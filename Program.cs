591647454f115120237915ede7c64ef1fdd2ab3d
Yasmim de Castro <Yas.castr0.r@gmail.com>
2025-03-31 08:43:17 -0300

    Finaliza Programa

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("Digite seu salário para calcularmos o seu FGTS: ");
+    double salario = Convert.ToDouble(Console.ReadLine()!);
+Console.WriteLine($"Salário (R$):{salario} ");
+    double fgts = 8 * (salario / 100);
+Console.WriteLine($"FGTS: {fgts} ");
